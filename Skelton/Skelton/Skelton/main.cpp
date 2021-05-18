a6fd0851ac655adbe8eed3e73a2a87cfb7659564
Tomohiro Inaki <daomuzhida@gmail.com>
2021-05-18 13:16:20 +0900

    キャラを表示

--- Skelton/Skelton/Skelton/main.cpp
+++ Skelton/Skelton/Skelton/main.cpp
@@ -1,8 +1,12 @@
 #include<DxLib.h>
+#include<cassert>
+
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	DxLib::SetGraphMode(1280, 720, 32);
 	DxLib::ChangeWindowMode(true);
 	DxLib::SetWindowText(L"Ninja Sprit");
+	auto graphH = DxLib::LoadGraph(L"../../../Asset/run3/adventurer-run3-sword-Sheet.png");
+	assert(graphH != -1);
 	if (DxLib_Init()) {
 		return -1;
 	}
@@ -10,6 +14,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	while (DxLib::ProcessMessage() == 0) {
 		DxLib::ScreenFlip();
 		DrawString(100, 100, L"hello wold",0xffffff);
+		DrawGraph(10, 10, graphH, true);
 		//drii
 	}
 	return 0;
